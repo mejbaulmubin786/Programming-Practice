62ca8a34c087e688eeb3fd8ed84d960d3b048e0d
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-08-29 07:51:13 +0600

    Make Connettion Using MySQLi OOP Style

--- DatabasePractice/Object-Oriented/connection.php
+++ DatabasePractice/Object-Oriented/connection.php
@@ -3,10 +3,10 @@
 require 'config.php';
 
 // কানেকশন তৈরি করা
-$conn = mysqli_connect($server, $user, $pass, $database);
+$conn = new mysqli($server, $user, $pass, $database);
 
 // কানেকশন চেক করা
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
