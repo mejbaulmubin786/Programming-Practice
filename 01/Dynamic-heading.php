3f9f2710e2162f1203e3f9f8e0dbc118a4bce551
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 04:54:01 +0600

    Basic Practice

---
+++ 01/Dynamic-heading.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP page practice</title>
+</head>
+<body>
+  <?php $text = "Hi this is a awesome website ";?>
+
+  <h1><?php echo "$text"; ?></h1>
+</body>
+</html>
