f9e80f4687c54201681a8f20aeabf9c9fa7e83d7
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 05:25:37 +0600

    Basic Practice

---
+++ 01/DynamicEmbadedList.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ol>
+    <?php
+$items = array(
+    "ফল" => array("আপেল", "কলা", "আম", "কমলা"),
+    "সবজি" => array("গাজর", "বাঁধাকপি", "টমেটো", "আলু"),
+    "অন্যান্য" => array("দুধ", "ডিম", "পাউরুটি", "চিনি"),
+);
+
+foreach ($items as $item) {
+    echo "<li> $item </li>";
+    echo "<ul>";
+    foreach ($item as $Sitem) {
+        echo "<li> $Sitem </li>";
+
+    }
+    echo "</ul>";
+}
+
+?>
+  </ol>
+</body>
+</html>
