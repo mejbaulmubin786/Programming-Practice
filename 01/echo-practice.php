2c3156af63e867c3468cc671d0c2904875a81eeb
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 07:20:55 +0600

    something

--- 01/echo-practice.php
+++ 01/echo-practice.php
@@ -1,2 +1,17 @@
 <?php
+$fname = "Mejbaul";
+$lname = "Mubin";
+$age = "32";
+$quallification = "BSc in Computer Science";
+$country = "Bangladesh";
+
 echo "Mejbaul Mubin";
+echo "Mejbaul", " ", "Mubin";
+
+echo "My name is $name";
+echo "I completed a {$quallification}";
+
+echo "$fname " . "$lname";
+
+echo "<h1>Welcome to my website</h1>";
+echo "<p>This is a paragraph.</p>";
