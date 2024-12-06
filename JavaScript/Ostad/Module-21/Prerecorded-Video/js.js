18bacaa0bb1e039f6f008017ec855757cb575eff
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-07 01:23:16 +0600

    Update

---
+++ JavaScript/Ostad/Module-21/Prerecorded-Video/js.js
@@ -0,0 +1,9 @@
+// 2. Hello Word
+document.write("Mejbaul Mubin");
+
+// 3. JS Comment
+// Single line Comment
+
+/*
+ multi line comment
+*/
---
+++ New folder/01.js
@@ -0,0 +1,20 @@
+const names = [
+  "HM Nayeem",
+  "Mejbaul Mubin",
+  "Aditya Chakraborty",
+  "Abu Rayhan",
+  "Shaker Hossain",
+  "Akib Ahmad",
+  "Alvi Chowdhury",
+];
+let index = -1;
+let person = names[++index];
+
+setInterval(() => {
+  person = names[index++];
+  console.log(person, person.length);
+
+  if (index === names.length) {
+    index = 0;
+  }
+}, 1000);
