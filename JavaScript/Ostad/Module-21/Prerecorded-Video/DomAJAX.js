0b8ee575f9c4d79c8025e529f0dbdae435bda1af
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-09 15:53:38 +0600

    42 HTML DOM Document

--- JavaScript/Ostad/Module-21/Prerecorded-Video/DomAJAX.js
+++ JavaScript/Ostad/Module-21/Prerecorded-Video/DomAJAX.js
@@ -1,3 +1,7 @@
+//----------------------------------------------------------------------------------41 Finding Elements By DOM
+document.write(
+  "<br>" + "<br>" + "<strong>41 Finding Elements By DOM</strong>" + "<br>"
+);
 var MyElement = document.getElementById("MyId");
 MyElement.innerHTML = "Is it really my First Dom practice";
 
@@ -6,3 +10,4 @@ MyElements[0].innerHTML = "Eid Mobarak";
 
 var MyPageElements = document.getElementsByName("MyName");
 MyPageElements[2].innerHTML = "Use By Name";
+//---------------------------------------------------------------------------------------42 HTML DOM Document
