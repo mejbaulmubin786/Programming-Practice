903b762bf41435a84ccd361a3dd6a322b285abe2
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-09 19:07:23 +0600

    AJAX practice

---
+++ JavaScript/Ostad/Module-21/Prerecorded-Video/3.AJAX.js
@@ -0,0 +1,11 @@
+//-----------------------------------------------------------------------------------------49 Ajax Introduction
+document.write(
+  "<br>" + "<br>" + "<strong>49 Ajax Introduction</strong>" + "<br>"
+);
+
+//AJAX PACKAGES
+//The XMLHttpRequest object build in with javascript
+//Fetch API supported by modern browser's
+//Axios package
+//JQuery Ajax
+//Superagent
