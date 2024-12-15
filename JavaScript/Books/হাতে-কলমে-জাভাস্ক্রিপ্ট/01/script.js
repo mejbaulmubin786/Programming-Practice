106af09d78905aeb7f100a650da314a6f5874cd8
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-16 02:47:08 +0600

    Page 79

--- JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/01/script.js
+++ JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/01/script.js
@@ -101,4 +101,73 @@ do {
   num += 1;
 } while (num < 10);
 
-console.log("Outsite the loop");
+console.log("Outsite do-while the loop");
+//break
+console.log(`break statement`);
+for (let i = 0; i < 10; i++) {
+  if (i === 5) break;
+  console.log(`i is now at: ${i}`);
+}
+//continue
+console.log(`continue statement`);
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    console.log(`${i} is skipped`);
+    continue;
+  }
+  console.log(`i is now at: ${i}`);
+}
+// --------------------------------------------------------------------------------------------Page-64
+let name = new Array(
+  "Mejbaul",
+  "Mubin",
+  "Rubel",
+  "Mejbaul Mubin",
+  "Mejbaul Mubin (Rubel)"
+);
+
+console.log(name);
+console.log(name[3]);
+console.log(name.length); // dot notation
+console.log(name["length"]); // bracket notation
+name.push("Sanzida"); //in last
+console.log(name);
+name.push("Islam");
+console.log(name);
+name.pop("Islam"); //from last
+console.log(name);
+name.shift(); //from first
+console.log(name);
+name.unshift("Mejbaul"); //in first
+console.log(name);
+let indx = name.indexOf("Mejbaul Mubin");
+console.log(indx);
+// --------------------------------------------------------------------------------------------Page-71
+let student = {
+  firstName: "Mejbaul",
+  lastName: "Mubin",
+  age: 35,
+  mobile: "01676880506",
+  university: "University of the people",
+};
+
+console.log(student);
+
+console.log(student.firstName);
+console.log("Full name: " + student.firstName + " " + student.lastName);
+student.birtyear = function (year) {
+  return year - this.age;
+};
+
+console.log(student.birtyear(2024));
+// --------------------------------------------------------------------------------------------Page-75
+console.log("------------------");
+for (item in student) {
+  console.log(student[item]);
+}
+
+console.log("------------------");
+for (item in student) {
+  console.log(student.item);
+}
+// --------------------------------------------------------------------------------------------Page-79
