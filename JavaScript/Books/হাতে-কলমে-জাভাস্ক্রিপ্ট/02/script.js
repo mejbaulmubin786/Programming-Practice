1f43394c4adb9690e4c9af418a5dbc8be718b1db
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-22 00:16:49 +0600

    Up and Going page 11

--- JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/02/script.js
+++ JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/02/script.js
@@ -15,3 +15,33 @@ function helloNewThis() {
 }
 
 helloNewThis();
+
+let myCustomObj = {
+  name: "Mejbaul Mubin",
+  age: 36,
+  job: "Jovern Ment Job",
+  msg: function () {
+    console.log(this);
+  },
+};
+
+myCustomObj.msg();
+
+let myCustomObj2 = {
+  name: "Mejbaul Mubin",
+  age: 36,
+  job: "Jovern Ment Job",
+  anotherObj: {
+    name: "Mejbaul Mubin Ruble",
+    msg: function () {
+      console.log("My name is: " + this.name);
+    },
+
+    value: function () {
+      console.log(this);
+    },
+  },
+};
+
+myCustomObj2.anotherObj.msg();
+myCustomObj2.anotherObj.value();
