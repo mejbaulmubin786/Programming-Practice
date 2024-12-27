073c507407a9eb3d0ca57671b0b202e8031f8de3
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-28 03:26:52 +0600

    Practice Dom

--- JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/05/script.js
+++ JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/05/script.js
@@ -1,4 +1,5 @@
 // -------------------------------------------------------------------------------------------Page-322
+/*
 console.log(document);
 console.log(document.querySelector("#box1"));
 
@@ -20,7 +21,25 @@ let tagSelector = document.querySelector("div");
 console.log(tagSelector);
 console.log(document.querySelectorAll("h4, p, ul"));
 console.log(document.getElementsByTagName("h4"));
+*/
+// -------------------------------------------------------------------------------------------Page-335
 
-// -------------------------------------------------------------------------------------------Page-103
+let selectABox = document.getElementById("box1");
+console.dir(selectABox);
+let newParagraph = document.createElement("p");
+console.log(newParagraph);
+let name = "Mejbaul Mubin";
+newParagraph.textContent = "His name is: " + name + "!";
+console.log(newParagraph);
+console.log(newParagraph.textContent);
+let newDiv = document.createElement("div");
+newDiv.innerHTML = "<p>DOM Manipulation is Fun!-Mubin</p>";
+console.log(newDiv);
+console.log(newDiv.innerHTML);
+newDiv.classList.add("new-div");
+console.log(newDiv);
 
-// -------------------------------------------------------------------------------------------Page-103
+// -------------------------------------------------------------------------------------------Page-344
+let box1 = document.getElementById("box1");
+box1.insertAdjacentElement("afterbegin", newDiv);
+box1.insertAdjacentElement("beforeend", newParagraph);
