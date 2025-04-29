da5790f4ebda7781c8628d41833a2cf9745b23a9
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-04-30 00:27:10 +0600

    update

--- JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/05/Dom/script.js
+++ JavaScript/Books/হাতে-কলমে-জাভাস্ক্রিপ্ট/05/Dom/script.js
@@ -1,2 +1,17 @@
-let box = document.querySelector("#box1");
-box.style.color = "blue";
+let box1 = document.getElementById("box1");
+let x = document.createElement("p");
+
+x.innerHTML = "Hello World!";
+
+box1.insertAdjacentElement("afterbegin", x);
+let y = document.createElement("p");
+
+y.innerHTML = "New Hello World!";
+box1.insertAdjacentElement("beforeend", y);
+
+let z = document.createElement("p");
+z.innerHTML = "New New Hello World!";
+let box2 = document.getElementById("box2");
+
+box2.insertAdjacentElement("beforebegin", z);
+box1.appendChild(box2);
